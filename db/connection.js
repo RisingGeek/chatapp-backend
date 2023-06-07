600f51f0be90ab41c7cf4f46efd92d33fcfb405c
RisingGeek <bhavay.puri@partsavatar.ca>
2023-06-07 23:45:28 +0530

    mongoose upgrade

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true}, () => {
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true }).then(() => {
     console.log('connected to databse');
 })
